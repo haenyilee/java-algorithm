93abe4c8b18e8116ab38caf1cbca4df4ddb70415
haenyilee <nee1202@naver.com>
2021-01-12 08:55:27 +0900

    DailyTemperature

--- src/me/haeni/string/DailyTemperature.java
+++ src/me/haeni/string/DailyTemperature.java
@@ -4,29 +4,27 @@ import java.util.Arrays;
 
 /*
 public class DailyTemperature {
-    public int[] dailyTemperatures(int[] temparatures) {
-        int[] result = null;
-        return result;
+    public int[] dailyTemperatures(int[] temperatures) {
     }
 }
  */
 public class DailyTemperature {
     public static void main(String[] args) {
-        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
-
+        int[] temperature = {73, 74, 75, 71, 69, 72, 76, 73};
         DailyTemperature dt = new DailyTemperature();
-        int[] result = dt.dailyTemperatures(temperatures);
+        int[] result = dt.dailyTemperatures(temperature);
         System.out.println(Arrays.toString(result));
     }
     public int[] dailyTemperatures(int[] temperatures) {
-        int[] result = null;
-        for(int i = 0; i<temperatures.length; i++){
-            int j = i+1;
-            int output =0;
-            if(temperatures[i]<temperatures[j]){
-                result[i] = output++;
+        int[] stack = new int[temperatures.length];
+        int[] result = new int[temperatures.length];
+        int top = -1;
+        for(int i = 0; i<temperatures.length; i++) {
+            while (top > -1 && temperatures[i] > temperatures[top]) {
+                int index = stack[top--];
+                result[index] = i - index;
             }
-
+            stack[++top] = i;
         }
         return result;
     }
---
+++ src/me/haeni/string/DailyTemperature_stack.java
@@ -0,0 +1,37 @@
+package me.haeni.string;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/*
+public class DailyTemperature {
+    public int[] dailyTemperatures(int[] temperatures) {
+    }
+}
+ */
+public class DailyTemperature_stack {
+    public static void main(String[] args) {
+        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+
+        DailyTemperature_stack dt = new DailyTemperature_stack();
+        int[] result = dt.dailyTemperatures(temperatures);
+        System.out.println(Arrays.toString(result));
+    }
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i<temperatures.length; i++) {
+            // stack의 가장 위에 있는 값이 i번째 온도보다 낮을 때
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                // stack에서 낮은 값을 빼고 stack에 담겨있던 i번호 저장
+                int index = stack.pop();
+                // result index번째에 결과 저장
+                result[index] = i - index;
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+}
